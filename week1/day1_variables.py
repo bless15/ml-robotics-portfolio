b6c40c31378ac681794ad779b222d82a0895e771
joseph <josephblessingibk@gmail.com>
2026-08-06 12:33:53 +0100

    Install NumPy and begin Week 2

---
+++ week1/day1_variables.py
@@ -0,0 +1,18 @@
+name = "Joseph"
+age = 21
+cgpa = 4.55
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print(cgpa)
+print(is_student)
+
+favourite_colour = "Blue"
+height = "183cm"
+country = "Nigeria"
+do_i_like_AI = True
+
+print(favourite_colour)
+print(height)
+print(country)
