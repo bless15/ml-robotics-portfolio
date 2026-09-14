eb3118cc3c41c76f47d88da249e537170113a786
joseph <josephblessingibk@gmail.com>
2026-09-14 14:53:31 +0100

    Clean up Week 6 PPO script

--- week6/ppo_cartpole.py
+++ week6/ppo_cartpole.py
@@ -1,29 +1,37 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 
+
+# Create CartPole environment
 env = gym.make("CartPole-v1")
 
+
+# Train PPO model
 model = PPO("MlpPolicy", env, verbose=1)
 
 model.learn(total_timesteps=10_000)
 
 
-#to measure its performance
+# Evaluate trained model
 obs, info = env.reset()
 
 total_reward = 0
-done = False
+terminated = False
+truncated = False
 
-while not done:
+while not terminated and not truncated:
+
     action, _states = model.predict(obs, deterministic=True)
+
     obs, reward, terminated, truncated, info = env.step(action)
 
     total_reward += reward
-    done = terminated or truncated
+
 
 print("Total reward:", total_reward)
 
-env.close()
 
-#save the trained model
+# Save trained model
 model.save("week6/ppo_cartpole")
+
+env.close()
